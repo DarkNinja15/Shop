ab158d854faa41ecab8c57cb9f95b61554280ac8
Animesh Shukla <animeshshukla1518@gmail.com>
2022-05-26 22:00:10 +0530

    first commit

---
+++ shop/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Shop',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('ShopApp'),
+      ),
+      body: Center(
+        child: Text('Lets build'),
+      ),
+    );
+  }
+}
---
+++ shop/lib/models/products.dart
@@ -0,0 +1,16 @@
+class Products {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+  bool isFavorite;
+  Products({
+    required this.id,
+    required this.title,
+    required this.description,
+    required this.price,
+    required this.imageUrl,
+    required this.isFavorite,
+  });
+}
